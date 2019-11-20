71df8690b5b9009943fd8d47340be02ff1037a20
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-20 15:48:34 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
 
-   epic_tragedy = {
-
+   epic_tragedy = {:hero => {name: "Romeo", age: "15", status: "dead"},
+    puts epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
