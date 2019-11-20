69994667a97298bec9983401a970b1135ec75fce
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-20 15:49:09 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
 
    epic_tragedy = {:hero => {name: "Romeo", age: "15", status: "dead"},
     puts epic_tragedy
- 
+   :capulet => {
  
  
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
